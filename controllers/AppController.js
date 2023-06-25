776812dd6a09b2782cea25bcdf33d85da605a074
Akinola <akinolatofunmi04@gmail>
2023-06-25 22:34:24 +0100

    Task 2 - Everything seems okay now

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -2,23 +2,22 @@
 import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
-export default class AppController {
-	static getStatus(req, res) {
-		if (redisClient.isAlive() & dbClient.isAlive()) {
-			res.send({redis: 'true', db: 'true'});
-			res.status(200);
-		}
-		res.status(400);
-	}
+class AppController {
+  static getStatus(req, res) {
+    if (redisClient.isAlive() & dbClient.isAlive()) {
+      res.send({ redis: 'true', db: 'true' });
+      res.status(200);
+    }
+    res.status(400);
+  }
 
-	static getStats(req, res) {
-		expectedRes = {
-			users: dbClient.nbUsers(),
-			files: dbClient.nbFiles()
-		}
-		res.send(expectedRes);
-		res.status(200)
-	}
+  static async getStats(req, res) {
+    const expectedRes = {
+      users: await dbClient.nbUsers(),
+      files: await dbClient.nbFiles()
+    }
+    res.send(expectedRes);
+    res.status(200)
+  }
 }
-
-// module.exports = AppController;
+module.exports = AppController;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 // import AppController from 'controllers/AppController'
-import AppController from 'controllers/AppController';
+import AppController from '../controllers/AppController';
 
 app.get('/status', AppController.getStatus);
 app.get('/stats', AppController.getStats)
