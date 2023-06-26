c421edc68b35fb85428849f272521c3bffb58f8e
Akinola <akinolatofunmi04@gmail>
2023-06-26 15:36:50 +0100

    Task 4 - Authenticate a user

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,6 +1,8 @@
 import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
 
 const crypto = require('crypto');
+const { ObjectId } = require('mongodb');
 
 class UsersController {
   static async postNew(req, res) {
@@ -30,6 +32,18 @@ class UsersController {
     await dbClient.db.collection('users').insertOne(newUser);
 
     return res.send({ id: newUser._id, email: newUser.email }).status(201);
+  }
+
+  static async getMe(req, res) {
+    const token = req.headers['x-token'];
+    const key = `auth_${token}`;
+    const userId = await redisClient.get(key);
+
+    const user = await dbClient.db.collection('users').findOne({ _id: ObjectId(userId) });
+    if (!user) {
+      return res.send({ error: 'Unauthorized' }).status(401);
+    }
+    return res.send({ email: user.email, id: user._id });
   }
 }
 module.exports = UsersController;
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@
 // import AppController from 'controllers/AppController'
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
 
 const express = require('express');
 
@@ -13,6 +14,11 @@ app.use(bodyParser.json());
 
 app.get('/status', AppController.getStatus);
 app.get('/stats', AppController.getStats);
+
+app.get('/connect', AuthController.getConnect);
+app.get('/disconnect', AuthController.getDisconnect);
+app.get('/users/me', UsersController.getMe);
+
 app.post('/users', UsersController.postNew);
 
 module.exports = app;
