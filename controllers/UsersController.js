a8e4a77555fe59a72e8b5616128cbc6d3cb30ecc
Akinola <akinolatofunmi04@gmail>
2023-07-03 12:52:16 +0100

    replace send with json to send res as json

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -11,18 +11,18 @@ class UsersController {
     const { password } = req.body;
 
     if (!email) {
-      return res.send({ error: 'Missing email' }).status(400);
+      return res.json({ error: 'Missing email' }).status(400);
     }
 
     if (!password) {
-      return res.send({ error: 'Missing password' }).status(400);
+      return res.json({ error: 'Missing password' }).status(400);
     }
     const hashedPwd = crypto.createHash('sha1').update(password).digest('hex');
 
     const user = await dbClient.db.collection('users').find({ email }).toArray();
     // console.log(user);
     if (user.length > 0) {
-      return res.send({ error: 'Already exist' }).status(400);
+      return res.json({ error: 'Already exist' }).status(400);
     }
 
     const newUser = {
@@ -31,7 +31,7 @@ class UsersController {
     };
     await dbClient.db.collection('users').insertOne(newUser);
 
-    return res.send({ id: newUser._id, email: newUser.email }).status(201);
+    return res.json({ id: newUser._id, email: newUser.email }).status(201);
   }
 
   static async getMe(req, res) {
@@ -41,9 +41,9 @@ class UsersController {
 
     const user = await dbClient.db.collection('users').findOne({ _id: ObjectId(userId) });
     if (!user) {
-      return res.send({ error: 'Unauthorized' }).status(401);
+      return res.json({ error: 'Unauthorized' }).status(401);
     }
-    return res.send({ id: user._id, email: user.email });
+    return res.json({ id: user._id, email: user.email }).status(200);
   }
 }
 module.exports = UsersController;
